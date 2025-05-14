90874f6f0341f28d08fc38d7bc2c19295bf1ed1c
eric <ebituin@gbox.adnu.edu.ph>
2025-05-14 20:00:51 +0800

    first commit

---
+++ api/index.js
@@ -0,0 +1,115 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import User from './models/user.js';
+import Post from './models/post.js';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+import multer from 'multer';
+const uploadMiddleware = multer({ dest: 'uploads/' });
+import fs from 'fs';
+
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const app = express();
+const salt = bcrypt.genSaltSync(10);
+const secret = 'your-secret-key'; // Add your secret key for JWT
+
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/uploads', express.static( __dirname + '/uploads'));
+
+mongoose.connect('mongodb+srv://ebituin:G27s%40edu%23194@cluster0.lzixnli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const userDoc = await User.create({ username, password: bcrypt.hashSync(password, salt) });
+        res.json(userDoc);
+    } catch (e) {
+        console.error('Registration error:', e);
+        res.status(400).json({ e });
+    }
+});
+
+app.post('/login', async (req, res) => {
+
+    try {
+        const { username, password } = req.body;
+        const userDoc = await User.findOne({ username });
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token).json({
+                    id: userDoc._id,
+                    username
+                });
+            });
+        } else {
+            res.status(400).json('wrong credentials');
+        }
+    } catch (err) {
+        console.error('Login error:', err);
+        res.status(400).json('wrong credentials');
+    }
+});
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req, res) => { res.cookie('token', '').json('ok') });
+
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { title, summary, content } = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover: newPath,
+            author: info.id,
+        });
+        res.json(postDoc);
+    });
+
+
+});
+
+app.get('/post', async (req, res) => {
+    const posts = await Post.find()
+    .populate('author', ['username'])
+    .sort({ createdAt: -1 })
+    .limit(20)
+    res.json(posts);
+});
+
+app.get('/post/:id', async (req, res) => {
+});
+
+app.listen(4000);
+
+//mongodb+srv://ebituin:G27s%40edu%23194@cluster0.lzixnli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 
---
+++ client/src/pages/indexPage.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import Post from "../post";
+
+export default function IndexPage() {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:4000/post').then(response => {
+            response.json().then(posts => {
+                setPosts(posts);
+                console.log(posts);
+            })
+        })
+    }, []);
+    return (
+        <>
+            {posts.length > 0 && posts.map(post => (
+                <Post key={post._id} {...post} />
+            ))}
+        </>
+    )
+}
